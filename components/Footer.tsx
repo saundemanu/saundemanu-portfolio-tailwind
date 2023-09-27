30870018b2c8d1d3112924d054c0c5474678449b
Emanuel Saunders <cs.emanuel42@gmail.com>
2023-09-27 16:10:21 -0400

    updated footer with proper links and names

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,7 @@ import {
   AiOutlineLinkedin,
   AiOutlineYoutube,
 } from "react-icons/ai"
+import { BsWordpress } from "react-icons/bs"
 
 const Footer = () => {
   return (
@@ -12,10 +13,10 @@ const Footer = () => {
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
       <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
         <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
-          © 2023 Hosna Qasmei<a href="/" className="hover:underline"></a>
+          © 2023 Emanuel Saunders<a href="/" className="hover:underline"></a>
         </div>
         <div className="flex flex-row items-center justify-center space-x-2 mb-1">
-          <a href="https://github.com/hqasmei" rel="noreferrer" target="_blank">
+          <a href="https://github.com/saundemanu" rel="noreferrer" target="_blank">
             <AiOutlineGithub
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
               size={30}
@@ -57,7 +58,7 @@ const Footer = () => {
           rel="nonreferrer"
           target="_blank"
           >
-            <AiOutlineWordpress
+            <BsWordpress
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
               size={30}
             />
